6fe49038b3120dc0ba30760bc263a7fd7e560de4
Ewelai <palevo.ewelai@gmail.com>
2019-04-04 00:43:59 +0300

    feat: add css configuration in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
 module.exports = {
   entry: {
     app: './src/index.js'
@@ -18,7 +21,19 @@ module.exports = {
         test: '/\.js$/',
         loader: 'babel-loader',
         exclude:'/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    })
+  ]
 }
